de97234ff661fbf12832ea8285b4aa89616582d6
NandhiniPandurangan151020 <nandhini151020@gmail.com>
2024-10-06 19:47:09 +0530

    Add files via upload

--- Data Science - Univariate/Univariate.py
+++ Data Science - Univariate/Univariate.py
@@ -8,3 +8,31 @@ class Univariate():
             else:
                 quan.append(columnName)
         return quan,qual  
+
+    def Descriptive():
+        descriptive=pd.DataFrame(index=["Mean","Median","Mode","Q1:25%","Q2:50%","Q3:75%","99%","Q4:100%","IQR","1.5rule","Lesser","Greater","Min","Max"],columns=quan)
+        for columnName in quan:
+            descriptive[columnName]["Mean"]=dataset[columnName].mean()
+            descriptive[columnName]["Median"]=dataset[columnName].median()
+            descriptive[columnName]["Mode"]=dataset[columnName].mode()[0]
+            descriptive[columnName]["Q1:25%"]=dataset.describe()[columnName]["25%"]
+            descriptive[columnName]["Q2:50%"]=dataset.describe()[columnName]["50%"]
+            descriptive[columnName]["Q3:75%"]=dataset.describe()[columnName]["75%"]
+            descriptive[columnName]["99%"]=np.percentile(dataset[columnName],99)
+            descriptive[columnName]["Q4:100%"]=dataset.describe()[columnName]["max"]
+            descriptive[columnName]["IQR"]=descriptive[columnName]["Q3:75%"]-descriptive[columnName]["Q1:25%"]
+            descriptive[columnName]["1.5rule"]=1.5*descriptive[columnName]["IQR"]
+            descriptive[columnName]["Lesser"]=descriptive[columnName]["Q1:25%"]-descriptive[columnName]["1.5rule"]
+            descriptive[columnName]["Greater"]=dataset.describe()[columnName]["75%"]+descriptive[columnName]["1.5rule"]
+            descriptive[columnName]["Min"]=dataset[columnName].min()
+            descriptive[columnName]["Max"]=dataset[columnName].max()
+        return Descriptive
+
+
+    def Frequency(columnName,dataset):
+        freqTable=pd.DataFrame(columns=["Unique_values","Frequency","Relative_Frequency","cumsum"])
+        freqTable["Unique_values"]=dataset[columnName].value_counts().index
+        freqTable["Frequency"]=dataset[columnName].value_counts().values
+        freqTable["Relative_Frequency"]=freqTable["Frequency"]/103
+        freqTable["cumsum"]=freqTable["Relative_Frequency"].cumsum()
+        return freqTable
